46232d8da57fe6e03321fbaba3a9be68dff69a2e
Nithin N P <nithin.n.p@pwc.com>
2023-05-08 15:45:02 +0530

    changed imports

--- SeleniumTest.java
+++ SeleniumTest.java
@@ -1,5 +1,5 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.seleniumhq.selenium.WebDriver;
+import org.seleniumhq.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
     public static void main(String[] args) {
